0e63edf6f8a1f95a9d41a21cb82d54735b951b3c
moises56 <moiavil56@gmail.com>
2021-07-22 23:22:32 -0600

    Componets, categ+header+inicio+perfil+signig+services

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private URL = 'http://localhost:3000/api'
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
+
+  signup(user:any){
+   return this.http.post<any>(this.URL + '/signup', user);
+  }
+
+  signIn(user:any){
+   return this.http.post<any>(this.URL + '/signin', user);
+  }
+
+  loggedIng(){
+    return !!localStorage.getItem('token');
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/signin'])
+  }
+
+
+}
